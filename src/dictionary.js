7359afacb5c54c34348673029535d85eddd8d054
Mansoor Amiri <mansooramiri@protonmail.com>
2025-09-12 13:47:31 +0100

    Add spool fragments, and update mask shard texts

--- src/dictionary.js
+++ src/dictionary.js
@@ -82,7 +82,7 @@ export function isItemUnlockedInPlayerSave(itemParsingInfo, saveData) {
  */
 export const collectables = [
 	{
-		name: "maskShards",
+		name: "Mask Shards",
 		necessity: "main",
 		tooltip: "Mask shards can increase your max HP. Each 4 shards count as 1%",
 		formula: (acquiredItems) => Math.floor(acquiredItems.length / 4),
@@ -100,8 +100,9 @@ export const collectables = [
 			{
 				name: "Mask Shard 2",
 				whichAct: 1,
-				prereqs: ["dash"],
-				location: "Wormways???",
+				prereqs: ["Swift Step"],
+				location:
+					"Behind a breakable wall near the simple key slot (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Crawl_02", "Heart Piece"],
@@ -110,8 +111,8 @@ export const collectables = [
 			{
 				name: "Mask Shard 3",
 				whichAct: 1,
-				prereqs: ["featherfall"],
-				location: "far fields near Shakra?",
+				prereqs: ["Drifter's cloak"],
+				location: "North-west of the Seamstress (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Bone_East_20", "Heart Piece"],
@@ -120,8 +121,9 @@ export const collectables = [
 			{
 				name: "Mask Shard 4",
 				whichAct: 1,
-				prereqs: ["featherfall"],
-				location: "shellwood?",
+				prereqs: ["Drifter's cloak"],
+				location:
+					"Shellwood - at the end of a platforming challenge behind a breakable wall",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Shellwood_14", "Heart Piece"],
@@ -130,8 +132,8 @@ export const collectables = [
 			{
 				name: "Mask Shard 5",
 				whichAct: 1,
-				prereqs: ["walljump"],
-				location: "west deep docks?",
+				prereqs: ["Wall Cling"],
+				location: "Deep docks, very close to the Marrow entrance (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Dock_08", "Heart Piece"],
@@ -140,8 +142,9 @@ export const collectables = [
 			{
 				name: "Mask Shard 6",
 				whichAct: 1,
-				prereqs: ["needolin"],
-				location: "Weavenest Atla?",
+				prereqs: ["Needolin"],
+				location:
+					"In a room behind a breakable wall on the east side of Weavenest Atla (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Weave_05b", "Heart Piece"],
@@ -151,7 +154,8 @@ export const collectables = [
 				name: "Mask Shard 7",
 				whichAct: 1,
 				prereqs: [],
-				location: "Savage Beastfly quest",
+				location:
+					"Reward for the Savage Beastfly quest obtainable in Bellhart (unverified)",
 				parsingInfo: {
 					type: "quest",
 					internalId: "Beastfly Hunt",
@@ -161,7 +165,8 @@ export const collectables = [
 				name: "Mask Shard 8",
 				whichAct: 2,
 				prereqs: [],
-				location: "between choral chambers/cogwork core?",
+				location:
+					"Leftmost part of Cogwork core behind an challenge room (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Song_09", "Heart Piece"],
@@ -171,7 +176,8 @@ export const collectables = [
 				name: "Mask Shard 9",
 				whichAct: 2,
 				prereqs: [],
-				location: "Whispering vaults?",
+				location:
+					"Whispering Vaults. At the top of a room. The movable box must be hit from beneath from another room behind a breakable ceiling (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Library_05", "Heart Piece"],
@@ -180,8 +186,9 @@ export const collectables = [
 			{
 				name: "Mask Shard 10",
 				whichAct: 2,
-				prereqs: ["clawline"],
-				location: "Bilewater?",
+				prereqs: ["Clawline"],
+				location:
+					"Bilewater. At the end of two long horizontal corridors (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Shadow_13", "Heart Piece"],
@@ -190,8 +197,9 @@ export const collectables = [
 			{
 				name: "Mask Shard 11",
 				whichAct: 2,
-				prereqs: ["clawline"],
-				location: "top of skull cavern?",
+				prereqs: ["Clawline"],
+				location:
+					"At the top of Skull Cavern, which is in the southeast of Far Fields (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Bone_East_LavaChallenge", "Heart Piece (1)"],
@@ -200,8 +208,9 @@ export const collectables = [
 			{
 				name: "Mask Shard 12",
 				whichAct: 2,
-				prereqs: ["clawline"],
-				location: "top of slab?",
+				prereqs: ["Clawline"],
+				location:
+					"The Slab - at the end of a platforming challenge which is behind a lock which needs the Key of Apostate (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Slab_17", "Heart Piece"],
@@ -210,8 +219,8 @@ export const collectables = [
 			{
 				name: "Mask Shard 13",
 				whichAct: 2,
-				prereqs: ["doublejump"],
-				location: "left of mount fay?",
+				prereqs: ["Faydown Cloak"],
+				location: "Left of the southwest bench in Mount Fay (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Peak_04c", "Heart Piece"],
@@ -220,8 +229,8 @@ export const collectables = [
 			{
 				name: "Mask Shard 14",
 				whichAct: 2,
-				prereqs: ["doublejump"],
-				location: "right of wisp thicket?",
+				prereqs: ["Faydown Cloak"],
+				location: "East part of the Wisp Thicket (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Wisp_07", "Heart Piece"],
@@ -230,18 +239,30 @@ export const collectables = [
 			{
 				name: "Mask Shard 15",
 				whichAct: 2,
-				prereqs: ["doublejump"],
-				location: "Jubilana?",
+				prereqs: ["Faydown Cloak"],
+				location:
+					"Purchased from Jubilana after saving her as part of the The Lost Merchant quest (unverified)",
 				parsingInfo: {
 					type: "flag",
 					internalId: "MerchantEnclaveShellFragment",
 				},
 			},
 			{
+				name: "Mask Shard 16",
+				whichAct: 2,
+				prereqs: ["Faydown Cloak"],
+				location:
+					"Bottom-left of blasted steps near the world border. Requires you to reach a certain platform via a jump while dashing -> Clawline -> double jump. (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Coral_19b", "Heart Piece"],
+				},
+			},
+			{
 				name: "Mask Shard 17",
 				whichAct: 3,
 				prereqs: [],
-				location: "Fastest in Pharloom quest",
+				location: "Reward for the Fastest in Pharloom quest (unverified)",
 				parsingInfo: {
 					type: "quest",
 					internalId: "Sprintmaster Race",
@@ -251,7 +272,7 @@ export const collectables = [
 				name: "Mask Shard 18",
 				whichAct: 3,
 				prereqs: [],
-				location: "Hidden Hunter quest",
+				location: "Reward for the Hidden Hunter quest (unverified)",
 				parsingInfo: {
 					type: "quest",
 					internalId: "Ant Trapper",
@@ -261,7 +282,7 @@ export const collectables = [
 				name: "Mask Shard 19",
 				whichAct: 3,
 				prereqs: [],
-				location: "Dark hearts quest",
+				location: "Reward for the Dark Hearts quest (unverified)",
 				parsingInfo: {
 					type: "quest",
 					internalId: "Destroy Thread Cores",
@@ -270,21 +291,209 @@ export const collectables = [
 			{
 				name: "Mask Shard 20",
 				whichAct: 3,
-				prereqs: ["superjump"],
-				location: "Left of blasted steps?",
-				parsingInfo: {
-					type: "sceneData",
-					internalId: ["Coral_19b", "Heart Piece"],
-				},
-			},
-			{
-				name: "Mask Shard 16",
-				whichAct: 3,
-				prereqs: ["superjump"],
-				location: "brightvein?",
+				prereqs: ["Silk Soar"],
+				location:
+					"Inside Brightvein, which is near the middle of Mount Fay and requires a super jump into a hidden ceiling passage. (unverified)",
 				parsingInfo: {
 					type: "sceneData",
 					internalId: ["Peak_06", "Heart Piece"],
+				},
+			},
+		],
+	},
+	{
+		name: "Spool Fragments",
+		necessity: "main",
+		tooltip:
+			"Spool fragments can increase your max silk. Each 2 shards count as 1%",
+		formula: (acquiredItems) => Math.floor(acquiredItems.length / 4),
+		items: [
+			{
+				name: "Spool Fragment 1",
+				whichAct: 1,
+				prereqs: [],
+				location: "Above bonebottom settlement",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Bone_11b", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 2",
+				whichAct: 1,
+				prereqs: [],
+				location: "Left part of Deep docks (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Dock_03c", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 3",
+				whichAct: 1,
+				prereqs: [],
+				location:
+					"Fleamaster reward - will always show as unobtained for now, while I figure out the save file flag.",
+				parsingInfo: {
+					type: "flag",
+					internalId: "fleaRewardPlaceholderTODO",
+				},
+			},
+			{
+				name: "Spool Fragment 4",
+				whichAct: 1,
+				prereqs: ["Cling Grip"],
+				location:
+					"Greymoor - at the top of the room just right of the Bell bench (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Greymoor_02", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 5",
+				whichAct: 1,
+				prereqs: ["Cling Grip"],
+				location:
+					"Middle-left of the Slab. In one of the outdoor rooms which have the cold effect from Mount Fay (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Peak_01", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 6",
+				whichAct: 1,
+				prereqs: ["Cling Grip"],
+				location: "Weavenest Atla (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Weave_11", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 7",
+				whichAct: 1,
+				prereqs: ["Needolin"],
+				location: "Purchased from Bellhart merchant (unverified)",
+				parsingInfo: {
+					type: "flag",
+					internalId: "PurchasedBelltownSpoolSegment",
+				},
+			},
+			{
+				name: "Spool Fragment 8",
+				whichAct: 2,
+				prereqs: [],
+				location: "Bottom-right of Cogwork Core (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Cog_07", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 9",
+				whichAct: 2,
+				prereqs: [],
+				location:
+					"Maybe the one near east Underworks, accessible via a lift after Trobbio (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Library_11b", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 10",
+				whichAct: 2,
+				prereqs: [],
+				location: "Top of the Grand Gate room (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Song_19_entrance", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 11",
+				whichAct: 2,
+				prereqs: [],
+				location:
+					"Underworks - Behind a challenge room nearby the Ventrica station (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Under_10", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 12",
+				whichAct: 2,
+				prereqs: [],
+				location: "Whiteward - bottom of lift shaft (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Ward_01", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 13",
+				whichAct: 2,
+				prereqs: [],
+				location: "Balm for The Wounded quest reward (unverified)",
+				parsingInfo: {
+					type: "quest",
+					internalId: "Save Sherma",
+				},
+			},
+			{
+				name: "Spool Fragment 14",
+				whichAct: 2,
+				prereqs: ["Clawline"],
+				location: "Top of the high halls (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Hang_03_top", "Silk Spool"],
+				},
+			},
+
+			{
+				name: "Spool Fragment 15",
+				whichAct: 2,
+				prereqs: ["Clawline"],
+				location:
+					"Far east of Deep docks, near the lower Far fields entrance (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Bone_East_13", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 16",
+				whichAct: 2,
+				prereqs: ["Faydown Cloak"],
+				location: "Left of Memorium (unverified)",
+				parsingInfo: {
+					type: "sceneData",
+					internalId: ["Arborium_09", "Silk Spool"],
+				},
+			},
+			{
+				name: "Spool Fragment 17",
+				whichAct: 2,
+				prereqs: ["Faydown Cloak"],
+				location: "Purchased from Grindle in Blasted Steps (unverified)",
+				parsingInfo: {
+					type: "flag",
+					internalId: "purchasedGrindleSpoolPiece",
+				},
+			},
+			{
+				name: "Spool Fragment 18",
+				whichAct: 2,
+				prereqs: ["Faydown Cloak"],
+				location:
+					"Purchased from Jubilana after saving her as part of the The Lost Merchant quest (unverified)",
+				parsingInfo: {
+					type: "flag",
+					internalId: "MerchantEnclaveSpoolPiece",
 				},
 			},
 		],
